c448181baf2366a39e19e50dfab44655be736c44
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-09 16:14:45 +0700

    [#16] - Setup basic SDK included in example app

---
+++ AndroidSurveySDK/src/main/java/co/nimblehq/surveyapi/TestSDKClass.kt
@@ -0,0 +1,9 @@
+package co.nimblehq.surveyapi
+
+class TestSDKClass {
+    companion object {
+        fun testFromSDK() : String {
+            return "this is from SDK"
+        }
+    }
+}
--- SurveySDKExample/app/src/main/java/co/nimblehq/surveysdk/example/ui/screens/MainActivity.kt
+++ SurveySDKExample/app/src/main/java/co/nimblehq/surveysdk/example/ui/screens/MainActivity.kt
@@ -1,10 +1,14 @@
 package co.nimblehq.surveysdk.example.ui.screens
 
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.activity.viewModels
+import co.nimblehq.surveyapi.TestSDKClass
 import co.nimblehq.surveysdk.example.databinding.ActivityMainBinding
 import co.nimblehq.surveysdk.example.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -13,4 +17,15 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         get() = { inflater -> ActivityMainBinding.inflate(inflater) }
 
     override val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        test()
+    }
+
+    private fun test() {
+        val tmp = TestSDKClass.testFromSDK()
+        Log.e("MainActivity","Test from SDK : $tmp")
+    }
+
 }
