6dbe2c9dc7c4df2182c57e53ad9bf7e9e0ac445b
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-11 00:22:36 +0700

    [#9] - add survey list and detail entity [#9] - add moshi for json parsing [#9] - remove test class

---
+++ survey-sdk/src/main/java/co/nimblehq/surveysdk/entity/SurveyListEntity.kt
@@ -0,0 +1,69 @@
+package co.nimblehq.surveysdk.entity
+
+import com.squareup.moshi.Json
+
+data class SurveyListEntity(
+    val data: List<SurveyListItem>,
+    val meta: Meta
+) {
+
+    data class SurveyListItem(
+        var sortKey: Int,
+        var id: String,
+        var type: String?,
+        var attributes: Attributes?,
+        var relationships: Relationships?
+    ) {
+        data class Attributes(
+            var title: String?,
+            var description: String?,
+
+            @Json(name = "thank_email_above_threshold")
+            var thankEmailAboveThreshold: String?,
+
+            @Json(name = "thank_email_below_threshold")
+            var thankEmailBelowThreshold: String?,
+
+            @Json(name = "is_active")
+            var isActive: Boolean?,
+
+            @Json(name = "cover_image_url")
+            var coverImageURL: String?,
+
+            @Json(name = "created_at")
+            var createdAt: String?,
+
+            @Json(name = "active_at")
+            var activeAt: String?,
+
+
+            @Json(name = "survey_type")
+            var surveyType: String?
+        )
+
+        data class Relationships(
+            val questions: Questions
+        ) {
+            data class Questions(
+                val data: List<QuestionsDatum>
+            ) {
+                data class QuestionsDatum(
+                    val id: String,
+                    val type: String
+                )
+            }
+        }
+    }
+
+    data class Meta(
+        val page: Int,
+        val pages: Int,
+
+        @Json(name = "page_size")
+        val pageSize: Int,
+
+        val records: Int
+    )
+}
+
+
