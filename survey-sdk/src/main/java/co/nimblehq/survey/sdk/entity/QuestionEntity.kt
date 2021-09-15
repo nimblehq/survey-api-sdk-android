443dbc38e7b4a4da43d0a1721c9dc9051ccee8ad
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-15 19:23:09 +0700

    [#9] Update entities ad data class

--- survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/QuestionEntity.kt
+++ survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/QuestionEntity.kt
@@ -6,48 +6,50 @@ import moe.banana.jsonapi2.JsonApi
 import moe.banana.jsonapi2.Resource
 
 @JsonApi(type = "question")
-open class QuestionEntity : Resource() {
+data class QuestionEntity(
     @field:Json(name = "text")
-    var text: String? = null
+    var text: String? = null,
 
     @field:Json(name = "help_text")
-    var helpText: String? = null
+    var helpText: String? = null,
 
     @field:Json(name = "display_order")
-    var displayOrder = 0
+    var displayOrder: Int = 0,
 
     @field:Json(name = "short_text")
-    var shortText: String? = null
+    var shortText: String? = null,
 
     @field:Json(name = "pick")
-    var pick: String? = null
+    var pick: String? = null,
 
     @field:Json(name = "display_type")
-    var displayType: String? = null
+    var displayType: String? = null,
 
     @field:Json(name = "is_mandatory")
-    var isMandatory: Boolean? = null
+    var isMandatory: Boolean? = null,
 
     @field:Json(name = "image_url")
-    var imageUrl: String? = null
+    var imageUrl: String? = null,
 
     @field:Json(name = "cover_image_url")
-    var coverImageUrl: String? = null
+    var coverImageUrl: String? = null,
 
     @field:Json(name = "cover_image_opacity")
-    var coverImageOpacity: Double? = null
+    var coverImageOpacity: Double? = null,
 
     @field:Json(name = "is_shareable_on_facebook")
-    var isShareableOnFacebook: Boolean? = null
+    var isShareableOnFacebook: Boolean? = null,
 
     @field:Json(name = "is_shareable_on_twitter")
-    var isShareableOnTwitter: Boolean? = null
+    var isShareableOnTwitter: Boolean? = null,
 
     @field:Json(name = "tag_list")
-    var tagList: String? = null
+    var tagList: String? = null,
 
     var answers: HasMany<AnswerEntity>? = null
 
+
+) : Resource() {
     fun getAnswerList(): List<AnswerEntity>? {
         return answers?.get(document)
     }
--- survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/SurveyEntity.kt
+++ survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/SurveyEntity.kt
@@ -6,36 +6,37 @@ import moe.banana.jsonapi2.JsonApi
 import moe.banana.jsonapi2.Resource
 
 @JsonApi(type = "survey")
-class SurveyEntity : Resource() {
+data class SurveyEntity(
     @field:Json(name = "title")
-    var title: String? = null
+    var title: String? = null,
 
     @field:Json(name = "description")
-    var description: String? = null
+    var description: String? = null,
 
     @field:Json(name = "thank_email_above_threshold")
-    var thankEmailAboveThreshold: String? = null
+    var thankEmailAboveThreshold: String? = null,
 
     @field:Json(name = "thank_email_below_threshold")
-    var thankEmailBelowThreshold: String? = null
+    var thankEmailBelowThreshold: String? = null,
 
     @field:Json(name = "is_active")
-    var isActive: Boolean? = null
+    var isActive: Boolean? = null,
 
     @field:Json(name = "cover_image_url")
-    var coverImageURL: String? = null
+    var coverImageURL: String? = null,
 
     @field:Json(name = "created_at")
-    var createdAt: String? = null
+    var createdAt: String? = null,
 
     @field:Json(name = "active_at")
-    var activeAt: String? = null
+    var activeAt: String? = null,
 
     @field:Json(name = "survey_type")
-    var surveyType: String? = null
+    var surveyType: String? = null,
 
     var questions: HasMany<QuestionEntity>? = null
 
+) : Resource() {
     fun getQuestionList(): List<QuestionEntity>? {
         val resources = questions?.get()
         return resources?.map { resource ->
@@ -45,5 +46,4 @@ class SurveyEntity : Resource() {
             }
         }
     }
-
 }
