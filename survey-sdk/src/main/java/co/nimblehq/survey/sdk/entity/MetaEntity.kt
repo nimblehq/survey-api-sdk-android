dedb63eb0146d6032d1ab30fc86c62472b255556
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-16 15:44:42 +0700

    [#9] Update block comment

--- survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/MetaEntity.kt
+++ survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/MetaEntity.kt
@@ -3,8 +3,8 @@ package co.nimblehq.survey.sdk.entity
 import com.squareup.moshi.Json
 
 /*
-This class not be included into json-api.
-Hence, we use @Json annotation for Parsing with Moshi Adapter
+ * This class not be included into json-api.
+ * Hence, we use @Json annotation for Parsing with Moshi Adapter
  */
 data class MetaEntity(
     @Json(name = "page")
