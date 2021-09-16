ab752908cd14c22c85ee2b59eac8f7bf8dc3120e
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-16 14:44:10 +0700

    [#9] Update comment why using @Json annotation in MetaEntity

--- survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/MetaEntity.kt
+++ survey-sdk/src/main/java/co/nimblehq/survey/sdk/entity/MetaEntity.kt
@@ -2,6 +2,10 @@ package co.nimblehq.survey.sdk.entity
 
 import com.squareup.moshi.Json
 
+/*
+This class not be included into json-api.
+Hence, we use @Json annotation for Parsing with Moshi Adapter
+ */
 data class MetaEntity(
     @Json(name = "page")
     val page: Int? = null,
