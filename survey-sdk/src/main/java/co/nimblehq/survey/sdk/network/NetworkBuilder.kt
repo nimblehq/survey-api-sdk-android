94acd5404b9376a037fc17a006316daaec13f032
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-17 09:18:57 +0700

    [#10] Create base network builder with Moshi Json Api support

--- survey-sdk/src/main/java/co/nimblehq/survey/sdk/network/NetworkBuilder.kt
+++ survey-sdk/src/main/java/co/nimblehq/survey/sdk/network/NetworkBuilder.kt
@@ -1,11 +1,8 @@
-package co.nimblehq.surveysdk.network
+package co.nimblehq.survey.sdk.network
 
-import co.nimblehq.surveysdk.api.AppService
-import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
 open class NetworkBuilder {
@@ -61,16 +58,7 @@ open class NetworkBuilder {
     ): Retrofit.Builder {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
-            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+            .addConverterFactory(MoshiBuilderProvider.getJsonApiConverterFactory())
     }
 
-    private fun provideMoshi(): Moshi = MoshiBuilderProvider.moshiBuilder.build()
-
-    protected fun buildAppService(): AppService {
-        return provideRetrofit().create(AppService::class.java)
-    }
-
-    inline fun <reified T> buildService(): T {
-        return provideRetrofit().create(T::class.java)
-    }
 }
