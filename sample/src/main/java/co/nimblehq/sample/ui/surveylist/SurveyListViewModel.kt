b988104f98f59d3e51484b26304d9e18987a3837
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-20 15:21:11 +0700

    [#7] Code refactor on SurveyListViewModel

--- sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
@@ -3,15 +3,15 @@ package co.nimblehq.sample.ui.surveylist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import co.nimblehq.survey.sdk.ResultSdk
+import co.nimblehq.survey.sdk.Result
 import co.nimblehq.survey.sdk.SurveyApi
 import co.nimblehq.survey.sdk.entity.SurveyEntity
 import kotlinx.coroutines.DelicateCoroutinesApi
 
 class SurveyListViewModel : ViewModel() {
 
-    private val _surveyListResult = MutableLiveData<ResultSdk<List<SurveyEntity>>>()
-    val surveyListResult: LiveData<ResultSdk<List<SurveyEntity>>> = _surveyListResult
+    private val _surveyListResult = MutableLiveData<Result<List<SurveyEntity>>>()
+    val surveyListResult: LiveData<Result<List<SurveyEntity>>> = _surveyListResult
 
     @DelicateCoroutinesApi
     fun getSurveyList() {
