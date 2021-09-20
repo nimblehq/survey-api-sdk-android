286453aaff6dbe4d2ed4e4f555d74dc06d44c6aa
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-20 13:55:01 +0700

    [#8] Update survey detail integration in ViewModel

--- sample/src/main/java/co/nimblehq/sample/ui/surveydetail/SurveyDetailViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/surveydetail/SurveyDetailViewModel.kt
@@ -3,26 +3,22 @@ package co.nimblehq.sample.ui.surveydetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import co.nimblehq.survey.sdk.ResultSdk
 import co.nimblehq.survey.sdk.SurveyApi
 import co.nimblehq.survey.sdk.entity.SurveyEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class SurveyDetailViewModel : ViewModel() {
 
     private val _surveyDetailResult = MutableLiveData<ResultSdk<SurveyEntity>>()
     val surveyDetailResult: LiveData<ResultSdk<SurveyEntity>> = _surveyDetailResult
 
+    @DelicateCoroutinesApi
     fun getSurveyDetail(id: String) {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val result = SurveyApi.instance.getSurveyDetail(id = id)
-                _surveyDetailResult.postValue(result)
-            }
-        }
+        SurveyApi.instance.getSurveyDetail(surveyId = id, onResponse = {
+            _surveyDetailResult.value = it
+        })
+
     }
 
 }
