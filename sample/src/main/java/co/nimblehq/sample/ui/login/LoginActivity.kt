b35be0509d5937469a8c4f695d9482a11b70f8df
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-20 22:05:49 +0700

    [#3] Remove unused code

--- sample/src/main/java/co/nimblehq/sample/ui/login/LoginActivity.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/login/LoginActivity.kt
@@ -2,13 +2,10 @@ package co.nimblehq.sample.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import co.nimblehq.sample.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,20 +13,6 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val login = binding.login
-
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-
-        })
 
     }
 
