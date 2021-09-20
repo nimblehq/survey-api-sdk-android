728085098571356d7a1adeb70bbaef8d6a16f11e
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-20 13:52:29 +0700

    [#7] Update survey list integration in ViewModel

--- sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
@@ -3,28 +3,21 @@ package co.nimblehq.sample.ui.surveylist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import co.nimblehq.survey.sdk.ResultSdk
 import co.nimblehq.survey.sdk.SurveyApi
 import co.nimblehq.survey.sdk.entity.SurveyEntity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 class SurveyListViewModel : ViewModel() {
 
     private val _surveyListResult = MutableLiveData<ResultSdk<List<SurveyEntity>>>()
     val surveyListResult: LiveData<ResultSdk<List<SurveyEntity>>> = _surveyListResult
 
+    @DelicateCoroutinesApi
     fun getSurveyList() {
-        // can be launched in a separate asynchronous job
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val result = SurveyApi.instance.getSurveyList(page = 1, size = 10)
-                _surveyListResult.postValue(result)
-            }
-        }
-
+        SurveyApi.instance.getSurveyList(page = 1, size = 10, onResponse = {
+            _surveyListResult.value = it
+        } )
     }
 
 }
