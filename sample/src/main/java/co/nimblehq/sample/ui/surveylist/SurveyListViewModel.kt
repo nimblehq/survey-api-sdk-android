7200a3aad3172bb90a8368ea7c7ef4aab91cc946
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-21 11:34:34 +0700

    [#4] Change UiModel to SurveyUiModel

--- sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
@@ -4,21 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import co.nimblehq.sample.ui.UiModel
+import co.nimblehq.sample.ui.SurveyUiModel
 import co.nimblehq.survey.sdk.entity.SurveyEntity
 import kotlinx.coroutines.launch
 
 class SurveyListViewModel : ViewModel() {
 
-    private val _surveyListResult = MutableLiveData<UiModel<SurveyListModel>>()
-    val surveyListUi: LiveData<UiModel<SurveyListModel>> = _surveyListResult
+    private val _surveyListResult = MutableLiveData<SurveyUiModel<SurveyListModel>>()
+    val surveyListSurveyUi: LiveData<SurveyUiModel<SurveyListModel>> = _surveyListResult
 
     fun getSurveyList() {
         // can be launched in a separate asynchronous job
         viewModelScope.launch {
             try {
                 //TODO: hardcode for testing, need to be integrate with SDK later
-                _surveyListResult.value = UiModel(success = SurveyListModel(listSurvey = mutableListOf(
+                _surveyListResult.value = SurveyUiModel(success = SurveyListModel(listSurvey = mutableListOf(
                     SurveyEntity("Survey 1", "Description 1"), SurveyEntity("Survey 2", "Description 2")
                 )))
             } catch (e: Exception) {
