14a81ba80c338d02c763459631f68e7f203ba33b
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-21 13:51:21 +0700

    [#4] Code change after rebasing

--- sample/src/main/java/co/nimblehq/sample/ui/ModelResult.kt
+++
@@ -1,6 +0,0 @@
-package co.nimblehq.sample.ui
-
-data class ModelResult<T> constructor(
-    val success: T? = null,
-    val error: Int? = null
-)
--- sample/src/main/java/co/nimblehq/sample/ui/login/LoginViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/login/LoginViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.nimblehq.sample.AuthService
-import co.nimblehq.sample.entity.UserEntity
 import co.nimblehq.survey.sdk.SurveyApi
 import co.nimblehq.survey.sdk.request.LoginRequest
 import kotlinx.coroutines.Dispatchers
@@ -14,8 +13,8 @@ import kotlinx.coroutines.withContext
 
 class LoginViewModel : ViewModel() {
 
-    private val _loginResult = MutableLiveData<Result<UserEntity>>()
-    val loginResult: LiveData<Result<UserEntity>> = _loginResult
+    private val _loginResult = MutableLiveData<Result<String>>()
+    val loginResult: LiveData<Result<String>> = _loginResult
 
     // As the SDK does not support Authentication service so far, then we will use custom service from the SDK
     val authenService = SurveyApi.instance.buildService<AuthService>()
@@ -28,8 +27,8 @@ class LoginViewModel : ViewModel() {
             withContext(Dispatchers.IO) {
                 try {
                     val result = authenService.loginEmail(request)
-                    SurveyApi.getInstance().setTokenApi(result.data.attributes.accessToken)
-                    _loginResult.postValue(ModelResult(success = UserModel("Hello!")))
+                    SurveyApi.instance.setTokenKey(result.data.attributes.accessToken)
+                    _loginResult.postValue(Result.success("Hello"))
 
                 } catch (exception: Exception) {
                     exception.printStackTrace()
