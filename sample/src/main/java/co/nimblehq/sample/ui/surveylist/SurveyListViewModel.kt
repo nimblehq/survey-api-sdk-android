bbef676930d79a535a80ca0296060e35ba219f68
Doan Dinh <dinhmanhdoan2008@gmail.com>
2021-09-21 17:52:18 +0700

    [#4] Add Handler to simulate api request

--- sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
+++ sample/src/main/java/co/nimblehq/sample/ui/surveylist/SurveyListViewModel.kt
@@ -1,5 +1,6 @@
 package co.nimblehq.sample.ui.surveylist
 
+import android.os.Handler
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -17,17 +18,21 @@ class SurveyListViewModel : ViewModel() {
     private val _surveyListResult = MutableLiveData<SurveyUiModel<List<SurveyModel>>>()
     val surveyListResult: LiveData<SurveyUiModel<List<SurveyModel>>> = _surveyListResult
 
-    private fun getSurveyList() {
+    fun getSurveyList() {
         // can be launched in a separate asynchronous job
         viewModelScope.launch {
             try {
-                //TODO: hardcode for testing, need to be integrate with SDK later
-                _surveyListResult.value = SurveyUiModel(
-                    success = mutableListOf(
-                        SurveyModel("0", "Survey 1", "Description 1"),
-                        SurveyModel("1", "Survey 2", "Description 2")
+                //TODO: use postDelayed to simulate calling request from server
+                Handler().postDelayed({
+                    //TODO: hardcode for testing, need to be integrate with SDK later
+                    _surveyListResult.value = SurveyUiModel(
+                        success = mutableListOf(
+                            SurveyModel("0", "Survey 1", "Description 1"),
+                            SurveyModel("1", "Survey 2", "Description 2")
+                        )
                     )
-                )
+                }, 2000)
+
             } catch (e: Exception) {
                 e.printStackTrace()
             }
